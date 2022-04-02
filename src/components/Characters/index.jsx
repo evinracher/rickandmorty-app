70c48524d7f6a8be9ef6aab23002d284e46c1cf2
evinracher <eevinley@gmail.com>
2022-04-02 14:48:12 -0500

    06: useReducer: como useState, pero más escalable

--- src/components/Characters/index.jsx
+++ src/components/Characters/index.jsx
@@ -1,11 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import styles from './Characters.module.css';
+
+const initialState = {
+  favorites: []
+};
+
+const favoriteReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITES':
+      console.log("Adding...");
+      return {
+        ...state,
+        favorites: [...state.favorites, action.payload]
+      };
+
+    default:
+      return state;
+  }
+};
 
 const API = 'https://rickandmortyapi.com/api/character';
 
 const Characters = () => {
   const [characters, setCharacters] = useState([]);
+  const [state, dispatch] = useReducer(favoriteReducer, initialState);
+
   const cardClass = `${styles.Characters__card}`;
+
+  const handleAddToFavorites = (favorite) => {
+    dispatch({ type: 'ADD_TO_FAVORITES', payload: favorite });
+  };
 
   useEffect(() => {
     fetch(API)
@@ -16,9 +40,15 @@ const Characters = () => {
 
   return (
     <div className={styles.Characters}>
+      {state.favorites.map(favorite => (
+        <div className={cardClass} key={favorite.id}>
+          <h2>{favorite.name}</h2>
+        </div>
+      ))}
       {characters.map(character => (
         <div className={cardClass} key={character.id}>
           <h2>{character.name}</h2>
+          <button onClick={() => handleAddToFavorites(character)}>Add to favorites</button>
         </div>
       ))}
     </div>
